93f67ccb8dcc91b1d39fd51ae803722e0f77b99e
MarcoVacchi <mvacchi816@gmail.com>
2025-08-23 17:32:19 +0200

    feat: added show auto

--- src/main/java/org/lessons/java/project_final_auto_java/controller/AutomobileController.java
+++ src/main/java/org/lessons/java/project_final_auto_java/controller/AutomobileController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,10 +20,17 @@ public class AutomobileController {
     private AutomobileRepository automobileRepository;
 
     @GetMapping
-
     public String index(Model model) {
         List<Automobile> automobile = automobileRepository.findAll();
         model.addAttribute("automobili", automobile);
         return "automobili/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+
+        Automobile automobile = automobileRepository.findById(id).get();
+        model.addAttribute("automobile", automobile);
+        return "automobili/show";
     }
 }
